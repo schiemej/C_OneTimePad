3a89e7c51a9820340f04bd92a0cd5bd1d73e5436
schiemej <55607998+schiemej@users.noreply.github.com>
2022-08-14 10:08:16 -0700

    Initial commit

---
+++ enc_client.c
@@ -0,0 +1,204 @@
+//Jacob Schiemenz
+//Assignment 5 One Time Pads
+//Due Date: 3-8-2021
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>  // ssize_t
+#include <sys/socket.h> // send(),recv()
+#include <netdb.h>      // gethostbyname()
+#include <ctype.h>
+#include <errno.h>
+
+/**
+* Client code
+* 1. Create a socket and connect to the server specified in the command arugments.
+* 2. Prompt the user for input and send that input as a message to the server.
+* 3. Print the message received from the server and exit the program.
+*/
+
+// Error function used for reporting issues
+void error(const char* msg) {
+    perror(msg);
+    exit(0);
+}
+
+// Set up the address struct
+void setupAddressStruct(struct sockaddr_in* address,
+    int portNumber,
+    char* hostname) {
+
+    // Clear out the address struct
+    memset((char*)address, '\0', sizeof(*address));
+
+    // The address should be network capable
+    address->sin_family = AF_INET;
+    // Store the port number
+    address->sin_port = htons(portNumber);
+
+    // Get the DNS entry for this host name
+    struct hostent* hostInfo = gethostbyname("localhost");
+    if (hostInfo == NULL) {
+        fprintf(stderr, "CLIENT: ERROR, no such host\n");
+        exit(0);
+    }
+    // Copy the first IP address from the DNS entry to sin_addr.s_addr
+    memcpy((char*)&address->sin_addr.s_addr,
+        hostInfo->h_addr_list[0],
+        hostInfo->h_length);
+}
+
+int main(int argc, char* argv[]) {
+    int count;
+    count = argc;
+    int socketFD, portNumber, charsWritten, charsRead;
+    struct sockaddr_in serverAddress;
+
+    //Holds the initial plaintext file
+    char buffer_0[71000] = {0};
+    char buffer_1[71000] = {0};
+    char buffer_2[142000] = { 0 };
+    
+    // Open plaintext file and get data
+    FILE* fp = fopen(argv[1], "r");
+    char* plaintext = NULL;
+    size_t ptSize;
+    int ptLength = getline(&plaintext, &ptSize, fp);
+    fclose(fp);
+    ptLength--;
+
+    int fp_count = ptLength;
+    int i;
+
+    for (i = 0; i < fp_count; i++) {
+        if ((isalpha(plaintext[i]) == 0) && (isspace(plaintext[i]) == 0)) {
+            perror("ILLEGAL CHARARCTER IN INPUT FILE...EXITING.\n");
+            fflush(stdout);
+            exit(1);
+        }
+    }
+    strncpy(buffer_0, plaintext, ptLength);
+
+    // Open key file and get data
+    fp = fopen(argv[2], "r");
+    char* key = NULL;
+    size_t keySize;
+    int keyLength = getline(&key, &keySize, fp);
+    fclose(fp);
+
+   int fp2_count = keyLength;
+
+   strncpy(buffer_1, key, keyLength);
+   //If key is shorter than file then action cannot be completed
+    if (fp_count > fp2_count) {
+        printf("ERROR: KEY IS SHORTER THAN PLAINTEXT FILE! ACTION CANNOT BE COMPLETED.\n");
+        fflush(stdout);
+        return 0;
+    }
+    //Combines all buffers into buffer 2 seperated by a newline character 
+    strcpy(buffer_2, buffer_0);
+    strcat(buffer_2, "\n");
+    strcat(buffer_2, buffer_1);
+
+    // Check usage & args
+    if (argc < 3) {
+        fprintf(stderr, "USAGE: %s hostname port\n", argv[0]);
+        exit(0);
+    }
+
+    // Create a socket
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD < 0) {
+        error("CLIENT: ERROR opening socket");
+    }
+
+    // Set up the server address struct
+    // Passes the LAST TWO arguments in argv into the address struct
+    setupAddressStruct(&serverAddress, atoi(argv[argc - 1]), argv[argc - 2]);
+
+    // Connect to server
+    if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        error("CLIENT: ERROR connecting");
+    }
+
+    int enc_code = 456;
+
+    //Send challenge to server
+    charsWritten = send(socketFD, &enc_code, sizeof(enc_code), 0);
+
+    int recv_check = 0;
+
+    //Recieve status code from server 
+    charsRead = recv(socketFD, &recv_check, sizeof(recv_check), 0);
+    if (recv_check != 0) {
+        goto SKIP_WORK;
+    }
+
+    int fp_full;
+    fp_full = fp_count + fp2_count;
+
+    //Send message length to server
+    charsWritten = send(socketFD, &fp_full, (sizeof(fp_full)), 0);
+
+
+    // Send message to server
+    // Write to the server
+
+    int write_count = 0;
+    int left_to_write = fp_full;
+    int loop_count = 0;
+    charsWritten = send(socketFD, buffer_2, fp_full, 0);
+    while (write_count < fp_full) {
+        charsWritten = send(socketFD, buffer_2, left_to_write, 0);
+        if (charsRead < 0) {
+            error("ERROR reading from socket\n");
+        }
+        else {
+            write_count += charsWritten;
+            left_to_write -= charsWritten;
+        }
+        loop_count++;
+        if (loop_count > 20) {
+            error("LOOP COUNT EXCEEDED\n");
+            break;
+        }
+    }
+    if (charsWritten < 0) {
+        error("CLIENT: ERROR writing to socket");
+    }
+    if (charsWritten < fp_count) {
+        printf("CLIENT: WARNING: Not all data written to socket!\n");
+    }
+
+    // Get return message from server
+    // Clear out the buffer again for reuse
+    memset(buffer_1, '\0', sizeof(buffer_1));
+
+    loop_count = 0;
+    int read_count;
+    read_count = 0;
+    int left_to_read;
+    left_to_read = fp_count;
+    
+    //While charsread is less than file length, continue to recieve from server.
+    int pos = 0;
+    while (pos < fp_count) {
+        charsRead = recv(socketFD, buffer_1 + pos, fp_count-pos, 0);
+        pos += charsRead;
+        //printf("CHARS READ: %d POS: %d FP_COUNT: %d\n", charsRead, pos, fp_count);
+        loop_count++;
+        if (loop_count > 20) {
+            error("LOOP COUNT EXCEEDED\n");
+            break;
+        }
+    }
+
+    //Prints output to stdout
+   printf("%s\n", buffer_1);
+SKIP_WORK:;
+    // Close the socket
+    close(socketFD);
+    return 0;
+}
---
+++ enc_server.c
@@ -0,0 +1,209 @@
+//Jacob Schiemenz
+//Assignment 5 One Time Pads
+//Due Date: 3-8-2021
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// Error function used for reporting issues
+void error(const char* msg) {
+    perror(msg);
+    exit(1);
+}
+
+// Set up the address struct for the server socket
+void setupAddressStruct(struct sockaddr_in* address,
+    int portNumber) {
+
+    // Clear out the address struct
+    memset((char*)address, '\0', sizeof(*address));
+
+    // The address should be network capable
+    address->sin_family = AF_INET;
+    // Store the port number
+    address->sin_port = htons(portNumber);
+    // Allow a client at any address to connect to this server
+    address->sin_addr.s_addr = INADDR_ANY;
+}
+
+int main(int argc, char* argv[]) {
+    int connectionSocket, charsRead;
+
+    // Hold the initial plaintext file the client sends
+    char buffer[140000 * sizeof(char)];
+    char buffer_1[70000 * sizeof(char)];
+    char buffer_2[70000 * sizeof(char)];
+
+    // Hold the key the client sends
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t sizeOfClientInfo = sizeof(clientAddress);
+
+    // Check usage & args
+    if (argc < 2) {
+        fprintf(stderr, "USAGE: %s port\n", argv[0]);
+        exit(1);
+    }
+
+    // Create the socket that will listen for connections
+    int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket < 0) {
+        error("ERROR opening socket");
+    }
+
+    // Set up the address struct for the server socket
+    setupAddressStruct(&serverAddress, atoi(argv[1]));
+
+    // Associate the socket to the port
+    if (bind(listenSocket,
+        (struct sockaddr*)&serverAddress,
+        sizeof(serverAddress)) < 0) {
+        error("ERROR on binding");
+    }
+
+    // Start listening for connetions. Allow up to 5 connections to queue up
+    listen(listenSocket, 5);
+
+    // Accept a connection, blocking if one is not available until one connects
+    while (1) {
+        // Accept the connection request which creates a connection socket
+        connectionSocket = accept(listenSocket,
+            (struct sockaddr*)&clientAddress,
+            &sizeOfClientInfo);
+
+        if (connectionSocket < 0) {
+            error("ERROR on accept");
+        }
+
+        //Holds the recv code unique to the enc client server relationship      
+        int recv_code = 0;
+        int enc_code = 456;
+        int send_check = 1;
+
+        //Challenge and Password
+        charsRead = recv(connectionSocket, &recv_code, sizeof(enc_code), 0);
+        if (recv_code != enc_code) {
+            //If recv code not equal to dec code then print error and skip servers work while continuing to listen.
+            perror("ENC SERVER CAN ONLY BE ACCESSED BY ENC CLIENT\n");
+            charsRead = send(connectionSocket, &send_check, (sizeof(send_check)), 0);
+            close(connectionSocket);
+            goto CONT_LISTEN;
+        }
+        //Else send a status of 0 indicating that code matches
+        else {
+            send_check = 0;
+            charsRead = send(connectionSocket, &send_check, (sizeof(send_check)), 0);
+        }
+        //Ensures buffers are zeroed
+        memset(buffer, '\0', sizeof(buffer));
+        memset(buffer_1, '\0', sizeof(buffer_1));
+        memset(buffer_2, '\0', sizeof(buffer_2));
+
+        int file1_length = 0;
+        //Recieves message length from client
+        charsRead = recv(connectionSocket, &file1_length, sizeof(file1_length), 0);
+
+
+        //Ensure buffer is cleared before recieving message
+        memset(buffer, '\0', sizeof(buffer));
+
+        //Init counters for recv looops
+        //loop_count used in error handling
+        int loop_count = 0;
+        //read_count keeps track of characters read
+        int read_count = 0;
+        //left_to_read keeps track of how many characters remain
+        int left_to_read = file1_length;
+
+        //Init pointer to buffer
+        char* buffer_cpy = buffer;
+        while (read_count < file1_length) {
+            charsRead = recv(connectionSocket, buffer_cpy, left_to_read, 0);
+            buffer_cpy += charsRead;
+            // If read_count is less than zero then that means that an error has occured and we close the program
+            if (read_count < 0) {
+                error("SERVER ERROR reading from socket\n");
+            }
+            // Else we increment the read_count and decrement the left_to_read count
+            else {
+                read_count += charsRead;
+                left_to_read -= charsRead;
+            }
+            //Keeps track of loops run and exits if over a certain amount.
+            //Stops the program from running into and infinite loop.
+            loop_count++;
+            if (loop_count > 20) {
+                error("LOOP COUNT EXCEEDED\n");
+                break;
+            }
+        }
+      
+        if (read_count < 0) {
+            error("ERROR reading from socket");
+        }
+
+        // Iterates through buffer and moves data into buffer_1 until it hits a newline
+        int y = 0;
+        while (buffer[y] != '\n') {
+            buffer_1[y] = buffer[y];
+            y++;
+        }
+
+        y++;
+        // Iterates through buffer and moves data into buffer_2 until it hits a newline
+        int z = 0;
+        while (buffer[z] != '\n') {
+            buffer_2[z] = buffer[y];
+            y++;
+            z++;
+        }
+        // Encrpyt file
+        int x;
+        //Iterates through plaintext file and performs decryption operations
+        for (x = 0; x < strlen(buffer_1) - 1; x++) {
+            //printf("ENCRYPTION LOOP: INDEX: %d BUFFER:%d\n", x, strlen(buffer_1));
+            if (buffer_1[x] == '\0') {
+                goto EXIT_LOOP;
+            }
+            //If buffer is equal to space
+            if (buffer_1[x] == 32) {
+                goto SKIP;
+            }
+            int a = buffer_1[x] - 65;
+            int b = buffer_2[x] - 65;
+            int c = a + b;
+
+            if (c > 25) {
+                c = c % 26;
+            }
+
+            buffer_1[x] = c + 65;
+        SKIP:;
+        }
+    EXIT_LOOP:;
+        //Send encrypted message to client
+        charsRead = send(connectionSocket, buffer_1, strlen(buffer_1), 0);
+        
+        if (charsRead < 0) {
+            error("ERROR writing to socket");
+        }
+        //Sleep for 1 second, leave this line in or else socket closes too early for client to recv all data
+        //Unexplained issued, worked on it with multiple TA's and this was the best solution found
+        sleep(1);
+        // Close the connection socket for this client
+        close(connectionSocket);
+
+        //Reset all buffers uses
+        memset(buffer, '\0', sizeof(buffer));
+        memset(buffer_1, '\0', sizeof(buffer_1));
+        memset(buffer_2, '\0', sizeof(buffer_2));
+    CONT_LISTEN:;
+    }
+    // Close the listening socket
+    close(listenSocket);
+    return 0;
+}
